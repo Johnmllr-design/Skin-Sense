b30c1813df4538b746da44815aec43354468c70e
John Miller <johnmiller@MacBookPro.grinnell.edu>
2025-09-16 01:44:13 -0500

    commiting updated UI

--- Skin Sense/ContentView.swift
+++ Skin Sense/ContentView.swift
@@ -13,37 +13,97 @@ import SwiftUI
 
 // post_user_info for posting the user information to the backend for
 // verification, and then returns an error or
-func post_user_info(user_name: String, pass_word: String) {
+
+
+
+func post_user_info(user_name: String, pass_word: String) async -> Bool {
     
-    print(user_name)
-    print(pass_word)
-    
-    // make a dictionary of values and keys
-    let JSON_dictionary: [String: Any] = ["username" : user_name, "password" : pass_word]
-    
-    // Convert dictionary to JSON object
-    let json_data = try! JSONSerialization.data(withJSONObject: JSON_dictionary)
-    guard let url = URL(string: "") else { return }
-            
+    let endpoint = "http://127.0.0.1:8000/login"   // or "http://localhost:8000/login"
+
+    // make a URL object from the endpoint string
+    guard let url = URL(string: endpoint) else {
+        print("Bad url")
+        return false
+    }
+
+    // make a body dictionary with the key information
+    let body: [String: Any] = ["username": user_name, "password": pass_word]
+
+    // convert the dictionary to a JSONserialization
+    guard let json = try? JSONSerialization.data(withJSONObject: body) else {
+        print("json serialization failed")
+        return false
+    }
+
+    // make a request objectfrom the url object
+    var req = URLRequest(url: url)
+
+    // establish the request hyperparameters
+    req.httpMethod = "POST"
+    req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    req.httpBody = json
+
+    // send and recieve API data
+    guard let (data, _) = try? await URLSession.shared.data(for: req) else {return false}
+    // decode the Data object into a dictionary with optionals
+    guard let dict = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+        return false
+    }
+    let result = (dict["result"] as? Int) == 1
+    print("the resulting login request is ", result)
+    return result
     
 }
 
 struct ContentView: View {
     @State var username:String = ""
     @State var password:String = ""
+    @State var login_result:Bool = true
+    @State var login_error:Bool = false
+    @State private var path:NavigationPath = NavigationPath()
+    
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 15).fill(Color.white.opacity(0.2))
-            RoundedRectangle(cornerRadius: 15).fill(Color.red).frame(width: 400, height: 100)
-            
-            VStack{
-                TextField("provide username", text:$username).padding()
-                TextField("provide password", text:$password).padding()
-                Button("login button"){
-                    post_user_info(user_name : username, pass_word : password)
+        NavigationStack(path: $path){
+            ZStack{
+                Rectangle().fill(Color.black)
+                RoundedRectangle(cornerRadius: 15).fill(Color.white).frame(width: 370, height: 270) // Specifies the size
+                VStack{
+                    TextField("provide a username", text:$username).frame(width:300, height: 100)
+                    TextField("provide a password", text:$password).frame(width:300, height: 100)
+                    Button("Log in"){
+                        Task{
+                            login_result = await post_user_info(user_name: username, pass_word: password)
+                            // if the login returns properly, log in
+                            if login_result == true {
+                                print("login result is ", login_result, " entering homescreen")
+                                path.append("HomeScreen")
+                                login_error = false
+                            }else{
+                                login_error = true
+                            }
+                        }
+                    }
                 }
             }
-            .padding()
+            .navigationDestination(for: String.self){value in
+                if value == "HomeScreen"{
+                    HomeScreen()
+                }
+            }
+            VStack{
+                if login_error{
+                    Text("erroneous login info")
+                }
+            }
+        }
+    }
+}
+
+struct HomeScreen: View {
+    var body: some View{
+        VStack{
+            Text("Welcome to skin sense!!!")
         }
     }
 }
